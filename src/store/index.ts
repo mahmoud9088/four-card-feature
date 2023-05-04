365ac55a116807f6af89f0c9b21ac438350bb1e0
Mahmoud Salman <salmanmahmoud70@gmail.com>
2023-05-04 21:09:45 +0300

    remove store data

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,57 +1,9 @@
-import { createStore } from "vuex";
+import { createStore } from 'vuex'
 
-
-interface FormData {
-  name: string,
-  email: string,
-  phone: string,
-  planOption: string,
-  addOnOptions: string[],
-}
-
-export interface PersonalInfo {
-  name: string,
-  email: string,
-  phone: string,
-}
-
-export interface RootState {
-  formData: FormData,
-  step: number
-}
-
-
-const storedData: any = localStorage.getItem("formData")
-
-
-export default createStore<RootState>({
-  state: {
-    formData: storedData ? JSON.parse(storedData) : {
-      name: "",
-      email: "",
-      phone: "",
-      planOption: "",
-      addOnOptions: [],
-    },
-    step: 1
-  },
+export default createStore({
+  state: {},
   getters: {},
-  mutations: {
-    toggleStep(state, step: number) {
-      state.step = step
-    },
-    addPersonalInfo(state, personalInfo: PersonalInfo) {
-      state.formData = { ...state.formData, ...personalInfo }
-      localStorage.setItem("formData", JSON.stringify(state.formData))
-    }
-  },
-  actions: {
-    toggleStep(context, step: number) {
-      context.commit('toggleStep', step)
-    },
-    addPersonalInfo(context, personalInfo: PersonalInfo) {
-      context.commit('addPersonalInfo', personalInfo)
-    },
-  },
+  mutations: {},
+  actions: {},
   modules: {},
-});
+})
