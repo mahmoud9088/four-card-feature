23373b5cedbc403eb222e1e9c28f1b30d6523a2f
Mahmoud Salman <salmanmahmoud70@gmail.com>
2023-05-04 21:08:14 +0300

    remove store data

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,6 @@
 <script lang="ts">
 import HomeView from './views/HomeView.vue';
 import { defineComponent } from 'vue';
-import "./assets/css/styles.css";
 
 export default defineComponent({
     components: { HomeView }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,37 +1,41 @@
 <template>
-    <section class="main">
-        <div class="container">
-            <div class="header">
-                <h1>Reliable, efficient delivery</h1>
-                <h2>Powered by Technology</h2>
-                <p>Our Artificial Intelligence powered tools use millions of project data points to ensure that your project is successful</p>
+  <section class="main">
+    <div class="container">
+      <div class="header">
+        <h1>Reliable, efficient delivery</h1>
+        <h2>Powered by Technology</h2>
+        <p>
+          Our Artificial Intelligence powered tools use millions of project data
+          points to ensure that your project is successful
+        </p>
+      </div>
 
-            </div>
-
-            <div class="cards">
-                <div v-for="card in cards" :key="card.title" :class="card.classes">
-                    <Card :title="card.title" :desc="card.desc" :icon="card.icon" :borderColor="card.borderColor" />
-                </div>
-            </div>
-
+      <div class="cards">
+        <div v-for="card in cards" :key="card.title" :class="card.classes">
+          <Card
+            :title="card.title"
+            :desc="card.desc"
+            :icon="card.icon"
+            :borderColor="card.borderColor"
+          />
         </div>
-
-    </section>
-
+      </div>
+    </div>
+  </section>
 </template>
 
 <script lang="ts">
-import Card from '@/components/Card.vue';
-import { defineComponent } from 'vue';
-import {cards} from '@/data/cards'
+import Card from '@/components/Card.vue'
+import { defineComponent } from 'vue'
+import { cards } from '@/data/cards'
 
 export default defineComponent({
-    name: "home-view",
-    components: { Card },
-    data(){
-        return{
-            cards
-        }
+  name: 'home-view',
+  components: { Card },
+  data() {
+    return {
+      cards,
     }
+  },
 })
 </script>
